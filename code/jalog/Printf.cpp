38ca5b33eb070ee3af98600ba7bcc962ad8e7ad8
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-11-23 06:02:09 +0200

    Printf variants

--- code/jalog/Printf.cpp
+++ code/jalog/Printf.cpp
@@ -7,25 +7,24 @@
 //
 #include "Printf.hpp"
 
+#include "Scope.hpp"
+
 #include <cstdio>
 #include <cstdarg>
 
 namespace jalog
 {
 
-void Printf(Scope& scope, Level lvl, const char* format, ...)
+void VPrintfUnchecked(Scope& scope, Level lvl, const char* format, va_list args)
 {
-    if (!scope.enabled(lvl)) return;
-
-    va_list args;
+    va_list acopy;
+    va_copy(acopy, args);
 
     // optimistically try to fit in a small buffer
     size_t size = 0;
     {
-        va_start(args, format);
         char buf[1024];
         size = size_t(vsnprintf(buf, sizeof(buf), format, args));
-        va_end(args);
 
         if (size <= sizeof(buf))
         {
@@ -36,11 +35,33 @@ void Printf(Scope& scope, Level lvl, const char* format, ...)
     }
 
     std::vector<char> dbuf(size + 1);
-    va_start(args, format);
-    size = size_t(vsnprintf(dbuf.data(), dbuf.size(), format, args));
-    va_end(args);
+    size = size_t(vsnprintf(dbuf.data(), dbuf.size(), format, acopy));
 
     scope.addEntryUnchecked(lvl, std::string_view(dbuf.data(), size));
+}
+
+void VPrintf(Scope& scope, Level lvl, const char* format, va_list args)
+{
+    if (!scope.enabled(lvl)) return;
+    VPrintfUnchecked(scope, lvl, format, args);
+}
+
+void Printf(Scope& scope, Level lvl, const char* format, ...)
+{
+    if (!scope.enabled(lvl)) return;
+
+    va_list args;
+    va_start(args, format);
+    VPrintfUnchecked(scope, lvl, format, args);
+    va_end(args);
+}
+
+void PrintfUnchecked(Scope& scope, Level lvl, const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    VPrintfUnchecked(scope, lvl, format, args);
+    va_end(args);
 }
 
 }
--- code/jalog/Printf.hpp
+++ code/jalog/Printf.hpp
@@ -8,10 +8,14 @@
 #pragma once
 #include "API.h"
 
-#include "Scope.hpp"
+#include "Level.hpp"
+
+#include <cstdarg>
 
 namespace jalog
 {
+
+class Scope;
 
 #if defined(__GNUC__)
 #   define I_JALOG_PRINTF_FMT __attribute__((format(printf, 3, 4)))
@@ -23,6 +27,11 @@ namespace jalog
 #   endif
 #endif
 
+
+JALOG_API void VPrintf(Scope& scope, Level lvl, const char* format, va_list args);
+JALOG_API void VPrintfUnchecked(Scope& scope, Level lvl, const char* format, va_list args);
+
 JALOG_API I_JALOG_PRINTF_FMT void Printf(Scope& scope, Level lvl, _Printf_format_string_ const char* format, ...);
+JALOG_API I_JALOG_PRINTF_FMT void PrintfUnchecked(Scope& scope, Level lvl, _Printf_format_string_ const char* format, ...);
 
 }
