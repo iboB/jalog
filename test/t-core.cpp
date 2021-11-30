267f10894118b2e51cfa22e1425d28cd14704549
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-11-30 06:37:25 +0200

    More tests

--- test/t-core.cpp
+++ test/t-core.cpp
@@ -20,16 +20,16 @@ TEST_SUITE_BEGIN("jalog");
 struct TestHelper
 {
     TestHelper()
-        : testScope(testLogger, "t1", 1, 2)
-        , testScope2(testLogger, "t2", 3, 4)
+        : scope(logger, "t1", 1, 2)
+        , scope2(logger, "t2", 3, 4)
         , sink(std::make_shared<TestSink>())
     {
-        testLogger.setup().add(sink);
+        logger.setup().add(sink);
     }
 
-    jalog::Logger testLogger;
-    jalog::Scope testScope;
-    jalog::Scope testScope2;
+    jalog::Logger logger;
+    jalog::Scope scope;
+    jalog::Scope scope2;
 
     std::shared_ptr<TestSink> sink;
 
@@ -47,20 +47,29 @@ struct TestHelper
         CHECK(e.scope.userData == 4);
     }
 
+    void checkChronologicalOrder()
+    {
+        auto& es = sink->entries;
+        CHECK(std::is_sorted(es.begin(), es.end(), [](auto& a, auto& b) {
+            return a.timestamp < b.timestamp;
+        }));
+    }
+
     TestSink::EntryCopy popFront()
     {
-        if (sink->entries.empty()) return {};
-        auto ret = std::move(sink->entries.front());
-        sink->entries.erase(sink->entries.begin());
+        auto& es = sink->entries;
+        if (es.empty()) return {};
+        auto ret = std::move(es.front());
+        es.erase(es.begin());
         return ret;
     }
 };
 
-#define tlog(lvl, ...) JALOG_SCOPE(helper.testScope, lvl, __VA_ARGS__)
+#define tlog(lvl, ...) JALOG_SCOPE(helper.scope, lvl, __VA_ARGS__)
 #define plog(...) \
     tlog(Info, __VA_ARGS__); \
     e = helper.popFront()
-#define tlog2(lvl, ...) JALOG_SCOPE(helper.testScope2, lvl, __VA_ARGS__)
+#define tlog2(lvl, ...) JALOG_SCOPE(helper.scope2, lvl, __VA_ARGS__)
 
 // logging to an unsetupped logger is safe and does nothing
 TEST_CASE("no setup")
@@ -80,7 +89,7 @@ TEST_CASE("no setup")
     CHECK(nope == 0);
 }
 
-TEST_CASE("levels")
+TEST_CASE("levels and scopes")
 {
     const uint64_t start = nanotime();
 
@@ -117,6 +126,9 @@ TEST_CASE("levels")
     tlog2(Error, "err");
     tlog(Critical, "crit");
 
+    REQUIRE(es.size() == 7);
+    helper.checkChronologicalOrder();
+
     {
         auto& e = es[3];
         helper.checkT2(e);
@@ -141,10 +153,122 @@ TEST_CASE("levels")
         CHECK(e.level == jalog::Level::Critical);
         CHECK(e.text == "crit");
     }
+}
 
-    REQUIRE(es.size() == 7);
+TEST_CASE("levels")
+{
+    TestHelper helper;
+    auto& es = helper.sink->entries;
 
-    CHECK(std::is_sorted(es.begin(), es.end(), [](auto& a, auto& b) { return a.timestamp < b.timestamp; }));
+    // shouldn't affect already created scopes
+    helper.logger.setDefaultLevel(jalog::Level::Off);
+
+    int argCounter = 0;
+    auto arg = [&]() { return argCounter++; };
+
+    auto logAll = [&]() {
+        tlog(Debug, arg());
+        tlog(Info, arg());
+        tlog(Warning, arg());
+        tlog(Error, arg());
+        tlog(Critical, arg());
+    };
+
+    logAll();
+    CHECK(argCounter == 5);
+    CHECK(es.size() == 5);
+
+    argCounter = 0;
+    es.clear();
+    helper.scope.setLevel(jalog::Level::Error);
+    logAll();
+    CHECK(argCounter == 2);
+    CHECK(es.size() == 2);
+
+    argCounter = 0;
+    es.clear();
+    helper.scope.setLevel(jalog::Level::Off);
+    for (int i=0; i<10; ++i) logAll();
+    CHECK(argCounter == 0);
+    CHECK(es.empty());
+
+    argCounter = 0;
+    es.clear();
+    helper.scope.setLevel(jalog::Level::Warning);
+    logAll();
+    CHECK(argCounter == 3);
+    CHECK(es.size() == 3);
+}
+
+TEST_CASE("new scopes")
+{
+    TestHelper helper;
+    tlog(Info, "foo");
+
+    {
+        helper.logger.setDefaultLevel(jalog::Level::Warning);
+        jalog::Scope newScope(helper.logger, "ns1", 10, 20);
+        JALOG_SCOPE(newScope, Debug, "dbg1"); // should be skipped
+        JALOG_SCOPE(newScope, Info, "info1"); // should be skipped
+        JALOG_SCOPE(newScope, Warning, "warn1");
+        tlog(Info, "bar");
+    }
+
+    {
+        helper.logger.setDefaultLevel(jalog::Level::Info);
+        jalog::Scope newScope(helper.logger, "ns2", 30, 40);
+        JALOG_SCOPE(newScope, Debug, "dbg2"); // should be skipped
+        JALOG_SCOPE(newScope, Info, "info2");
+        JALOG_SCOPE(newScope, Warning, "warn2");
+        tlog(Debug, "baz");
+    }
+
+    auto& es = helper.sink->entries;
+    CHECK(es.size() == 6);
+    helper.checkChronologicalOrder();
+
+    {
+        auto e = helper.popFront();
+        helper.checkT1(e);
+        CHECK(e.level == jalog::Level::Info);
+        CHECK(e.text == "foo");
+    }
+    {
+        auto e = helper.popFront();
+        CHECK(e.scope.label() == "ns1");
+        CHECK(e.scope.id() == 10);
+        CHECK(e.scope.userData == 20);
+        CHECK(e.level == jalog::Level::Warning);
+        CHECK(e.text == "warn1");
+    }
+    {
+        auto e = helper.popFront();
+        helper.checkT1(e);
+        CHECK(e.level == jalog::Level::Info);
+        CHECK(e.text == "bar");
+    }
+    {
+        auto e = helper.popFront();
+        CHECK(e.scope.label() == "ns2");
+        CHECK(e.scope.id() == 30);
+        CHECK(e.scope.userData == 40);
+        CHECK(e.level == jalog::Level::Info);
+        CHECK(e.text == "info2");
+    }
+    {
+        auto e = helper.popFront();
+        CHECK(e.scope.label() == "ns2");
+        CHECK(e.scope.id() == 30);
+        CHECK(e.scope.userData == 40);
+        CHECK(e.level == jalog::Level::Warning);
+        CHECK(e.text == "warn2");
+    }
+    {
+        auto e = helper.popFront();
+        helper.checkT1(e);
+        CHECK(e.level == jalog::Level::Debug);
+        CHECK(e.text == "baz");
+    }
 }
 
 struct vec { float x, y; };
