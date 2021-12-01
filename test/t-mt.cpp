6f1e8b4048dc05daaac3ec4cb335633955e33ff5
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-12-01 08:25:36 +0200

    Multi-threaded test stub

---
+++ test/t-mt.cpp
@@ -0,0 +1,17 @@
+// jalog
+// Copyright (c) 2021 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#include <doctest/doctest.h>
+
+#include "TestSink.hpp"
+
+TEST_SUITE_BEGIN("jalog");
+
+TEST_CASE("mt")
+{
+
+}
