d68d5c9128da1a0618c67a1ee419408f0b92a102
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-12-06 05:33:47 +0200

    HelloWorld example

---
+++ example/e-HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <jalog/Instance.hpp>
+#include <jalog/sinks/ColorSink.hpp>
+#include <jalog/Log.hpp>
+
+int main()
+{
+    jalog::Instance jl;
+    jl.setup().add<jalog::sinks::ColorSink>();
+
+    JALOG(Debug, "Perparing to greet world");
+    JALOG(Info, "Hello, world");
+
+    return 0;
+}
