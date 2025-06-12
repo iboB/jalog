ec64c8ff123e132b3e0fffdf40c47eb59390ba05
Borislav Stanimirov <b@ibob.bg>
2025-06-12 12:04:59 +0300

    bench: initial setup

---
+++ bench/b-core.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+#include <vector>
+#include <cstdlib> // for rand
+
+#include <picobench/picobench.hpp>
+
+#include <jalog/log.hpp>
+
+
